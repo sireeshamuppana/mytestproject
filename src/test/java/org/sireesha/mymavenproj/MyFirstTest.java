7fd37381d14d57fe301efc02c7f74cc81645603c
sirisha <dummysirisha@gmail.com>
2017-08-26 17:10:32 +0530

    added second line

--- src/test/java/org/sireesha/mymavenproj/MyFirstTest.java
+++ src/test/java/org/sireesha/mymavenproj/MyFirstTest.java
@@ -4,6 +4,7 @@ public class MyFirstTest {
 
 	public static void main(String[] args) {
 		System.out.println("THis is my first learining git project");
+		System.out.println("added second line of code");
 	}
 	
 }
